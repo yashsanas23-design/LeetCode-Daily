f9177dce00b192a7b587bc18503515a79017edb7
yashsanas23-design <yashsanas23@gmail.com>
2026-07-16 18:10:06 +0530

    Time: 1 ms (55.95%), Space: 43.2 MB (33.21%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,45 +1,45 @@
-class Solution {
-
-    public int countSubarrays(int nums[], int maxSum) {
+class Solution 
+{
+    public int countSubarrays(int nums[], int maxSum)
+     {
         int subarray = 1;
         int currentSum = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-
-            if (currentSum + nums[i] <= maxSum) {
+        for (int i = 0; i < nums.length; i++)
+         {
+            if (currentSum + nums[i] <= maxSum) 
+            {
                 currentSum += nums[i];
-            } else {
+            } 
+            else 
+            {
                 subarray++;
                 currentSum = nums[i];
             }
         }
-
         return subarray;
     }
-
-    public int splitArray(int[] nums, int k) {
-
+    public int splitArray(int[] nums, int k) 
+    {
         int low = 0;
         int high = 0;
-
-        for (int num : nums) {
+        for (int num : nums) 
+        {
             low = Math.max(low, num);
             high += num;
         }
-
-        while (low <= high) {
-
+        while (low <= high) 
+        {
             int mid = low + (high - low) / 2;
-
             int subarray = countSubarrays(nums, mid);
-
-            if (subarray > k) {
+            if (subarray > k)
+            {
                 low = mid + 1;
-            } else {
+            }
+             else
+            {
                 high = mid - 1;
             }
         }
-
         return low;
     }
 }
