828a7eee16fe870c15d26fefead49b5b8ca84164
yashsanas23-design <yashsanas23@gmail.com>
2026-09-08 14:47:01 +0530

    Time: 3 ms (35.16%), Space: 61.6 MB (72.7%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,33 +1,23 @@
 class Solution 
 {
-    public int maxScore(int[] cardPoints, int k) 
+    public int maxScore(int[] cardPoints, int k)
     {
-        int leftSum = 0;
-        int rightSum = 0;
-        int maxSum = 0;
-
-        // Take first k cards
-        for(int i = 0; i < k; i++)
+        int leftSum=0;
+        int rightSum=0;
+        int maxSum=0;
+        for(int i=0;i<k;i++)
         {
-            leftSum = leftSum + cardPoints[i];
+            leftSum=leftSum+cardPoints[i];
         }
-
-        maxSum = leftSum;
-
-        int rightIndex = cardPoints.length - 1;
-
-        // Gradually replace left cards with right cards
-        for(int i = k - 1; i >= 0; i--)
+        maxSum=leftSum;
+        int rightIndex=cardPoints.length-1;
+        for(int i=k-1;i>=0;i--)
         {
-            leftSum = leftSum - cardPoints[i];
-
-            rightSum = rightSum + cardPoints[rightIndex];
-
-            rightIndex--;
-
-            maxSum = Math.max(maxSum, leftSum + rightSum);
+            leftSum=leftSum-cardPoints[i];
+            rightSum=rightSum+cardPoints[rightIndex];
+            rightIndex=rightIndex-1;
+            maxSum=Math.max(leftSum+rightSum,maxSum);
         }
-
-        return maxSum;
+      return maxSum;
     }
 }
